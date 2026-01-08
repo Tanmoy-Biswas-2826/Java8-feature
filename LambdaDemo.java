3f2d755412f46dc0d1f5a6b8143dc2b1d6076995
Tanmoy-Biswas-2826 <tanmoybiswas2826@gmail.com>
2026-01-09 00:27:38 +0530

    fix: about lambda exprestion

--- LambdaDemo.java
+++ LambdaDemo.java
@@ -1,5 +1,39 @@
+import java.lang.invoke.CallSite;
+import java.util.List;
+
 public class LambdaDemo {
     public static void main(String[] args) {
-        System.out.println("About lamda");
+        /*
+            (parameters) -> { body }
+            () -> System.out.println("Hello");
+            (a, b) -> a + b;
+            x -> x * x;
+        */
+
+        //Before java 8
+        Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Running...");
+                }
+        };
+
+        //Java 8 use
+        Runnable r = ()-> System.out.println("Running...");
+
+        //Lambda with parameter
+        Calc c = (a,b) -> a+b;
+        System.out.println(c.add(5,3));
+
+        //Lambda with Collection
+        List<String> list = List.of("Java", "Spring", "Angular");
+        list.forEach(s -> System.out.println(s));
+        list.forEach(System.out::println); //Use Method reference
+        
     }
+}
+
+@FunctionalInterface
+interface Calc{
+    int add (int a, int b);
 }
